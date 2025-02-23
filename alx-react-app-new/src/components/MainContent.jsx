bb11958d431873320143defed03e0594efa7e46c
Lyndsay Takaindisa <lyndsaytakaindisa@gmail.com>
2025-02-23 16:14:12 +0200

    Applying Inline CSS Stylinh to React Components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const  MainContent = () => {
+  return(
+    <main style={{ backgroundColor: 'orangered',  fontSize : '34' }}>
+      <p style={{ color: 'purple' }}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+
+  );
+};
+
+export default MainContent;
