b8e94c30a3a7e0aebd3b4f2c4f4fe0d44a847a34
Lyndsay Takaindisa <lyndsaytakaindisa@gmail.com>
2025-02-23 21:14:20 +0200

    Fixed UserProfile to use Hooks instead of props

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React ,{useContext}from 'react'
+import UserContext from './UserContext'
 
-const UserProfile = (props) => {
+
+const UserProfile = () => {
+  const userContext = useContext(UserContext);
     return (
       <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
+        <h2>{userContext.name}</h2>
+        <p>Age: {userContext.age}</p>
+        <p>Bio: {userContext.bio}</p>
       </div>
     );
   };
